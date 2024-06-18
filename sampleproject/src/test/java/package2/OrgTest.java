0b9ea408cdd607377f2ea547bb83c5e66be4d821
poovizhi <sathyarajpoovizhi@gmail.com>
2024-06-18 14:45:14 +0530

    j

--- sampleproject/src/test/java/package2/OrgTest.java
+++ sampleproject/src/test/java/package2/OrgTest.java
@@ -7,6 +7,14 @@ public class OrgTest {
 	@Test
 	public void createorgTest() {
 		System.out.println("executeorgtest");
+		String url=System.getProperty("url");
+		String browser=System.getProperty("browser","chrome");
+		String username=System.getProperty("username");
+		String password=System.getProperty("password");
+		System.out.println(url);
+		System.out.println(browser);
+		System.out.println(username);
+		System.out.println(password);
 	}
 	@Test
 	public void modifyorgTest() {
